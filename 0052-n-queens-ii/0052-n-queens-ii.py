c1ed043f8d4b8146577b9eec1226096f9c0aec05
Pavandeep6420 <pavandeepreddy123@gmail.com>
2026-09-17 19:37:56 +0530

    Time: 6 ms (91.12%), Space: 19.3 MB (83.41%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        positive_diag = set()  # (r + c)
+        negative_diag = set()  # (r - c)
+
+        count = 0
+
+        def backtrack(r):
+            nonlocal count
+            if r == n:
+                count += 1
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in positive_diag or (r - c) in negative_diag:
+                    continue
+
+                col.add(c)
+                positive_diag.add(r + c)
+                negative_diag.add(r - c)
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                positive_diag.remove(r + c)
+                negative_diag.remove(r - c)
+
+        backtrack(0)
+        return count
