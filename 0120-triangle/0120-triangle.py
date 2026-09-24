96fade98d6f11fe4c60042afcea7350e923fb9d3
Pavandeep6420 <pavandeepreddy123@gmail.com>
2026-09-24 15:43:04 +0530

    Time: 0 ms (100%), Space: 20.2 MB (38.79%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+        # Start from the second-to-last row and move upwards
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                # Add the minimum of the two adjacent positions below
+                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+                
+        return triangle[0][0]
